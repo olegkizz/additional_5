ece585db2d1072b258aed1c1d9050d5ab87f9160
olegkizz <olegkizz1998@gmail.com>
2018-02-28 17:09:07 +0300

    Created Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var keys = [];
+	
+  	for (var el of str)
+  	{
+  		var key = false;
+  		for(var i = 0;i < bracketsConfig.length; ++i)
+  		{
+  			if(el == bracketsConfig[i][0]) 
+  				{	
+  					keys.push(i);
+  					key = true;
+  					break;	
+  				}
+  		}
+  		
+  		if(keys.length >= 2)
+  			if(bracketsConfig[keys[keys.length - 1]][1] == bracketsConfig[keys[keys.length - 2]][0] && bracketsConfig[keys[keys.length - 1]][0] == bracketsConfig[keys[keys.length - 2]][0])
+  						{
+  							keys.pop();
+  							keys.pop();
+  						}
+  			if(!key)
+  				{
+  					if(keys.length == 0) return false;
+
+  						else if (el != bracketsConfig[keys.pop()][1])
+  						{
+	
+  							return false;
+  						}
+  				}
+  	}
+	return keys.length == 0;
 }
